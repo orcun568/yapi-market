06e6abfda982e2a13616f08f784d55b002b6c695
orcun568 <orcun.akdogan61@gmail.com>
2025-06-03 13:41:02 +0300

    hata

--- src/app/api/addresses/route.js
+++ src/app/api/addresses/route.js
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "../auth/[...nextauth]/options";
+import { authOptions } from "@app/api/auth/[...nextauth]/options";
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
